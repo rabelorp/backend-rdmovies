3a8f5b302fe2f471aa2fb735bb48e91e05e7011c
Robson Rabelo <robson.rabelo@coopersystem.com.br>
2024-11-09 10:33:56 -0300

    feat: nestjs environment + features

---
+++ src/database/seeds/relational/user/user-seed.service.ts
@@ -0,0 +1,78 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { Repository } from 'typeorm';
+import bcrypt from 'bcryptjs';
+import { RoleEnum } from '../../../../roles/roles.enum';
+import { StatusEnum } from '../../../../statuses/statuses.enum';
+import { UserEntity } from '../../../../users/infrastructure/persistence/relational/entities/user.entity';
+
+@Injectable()
+export class UserSeedService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private repository: Repository<UserEntity>,
+  ) {}
+
+  async run() {
+    const countAdmin = await this.repository.count({
+      where: {
+        role: {
+          id: RoleEnum.admin,
+        },
+      },
+    });
+
+    if (!countAdmin) {
+      const salt = await bcrypt.genSalt();
+      const password = await bcrypt.hash('secret', salt);
+
+      await this.repository.save(
+        this.repository.create({
+          firstName: 'Robson',
+          lastName: 'Rabelo',
+          email: 'admin@rabelodigital.com',
+          password,
+          role: {
+            id: RoleEnum.admin,
+            name: 'Admin',
+          },
+          status: {
+            id: StatusEnum.active,
+            name: 'Active',
+          },
+        }),
+      );
+    }
+
+    const countUser = await this.repository.count({
+      where: {
+        role: {
+          id: RoleEnum.user,
+        },
+      },
+    });
+
+    if (!countUser) {
+      const salt = await bcrypt.genSalt();
+      const password = await bcrypt.hash('secret', salt);
+
+      await this.repository.save(
+        this.repository.create({
+          firstName: 'Robson',
+          lastName: 'Rabelo',
+          email: 'user@rabelodigital.com',
+          password,
+          role: {
+            id: RoleEnum.user,
+            name: 'User',
+          },
+          status: {
+            id: StatusEnum.active,
+            name: 'Active',
+          },
+        }),
+      );
+    }
+  }
+}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,43 @@
+import { Transform, Type } from 'class-transformer';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsOptional, MinLength } from 'class-validator';
+import { FileDto } from '../../files/dto/file.dto';
+import { RoleDto } from '../../roles/dto/role.dto';
+import { StatusDto } from '../../statuses/dto/status.dto';
+import { lowerCaseTransformer } from '../../utils/transformers/lower-case.transformer';
+
+export class CreateUserDto {
+  @ApiProperty({ example: 'test1@rabelodigital.com', type: String })
+  @Transform(lowerCaseTransformer)
+  @IsNotEmpty()
+  @IsEmail()
+  email: string | null;
+
+  @ApiProperty()
+  @MinLength(6)
+  password?: string;
+
+  provider?: string;
+
+  @ApiProperty({ example: 'Robson', type: String })
+  @IsNotEmpty()
+  firstName: string | null;
+
+  @ApiProperty({ example: 'Rabelo', type: String })
+  @IsNotEmpty()
+  lastName: string | null;
+
+  @ApiPropertyOptional({ type: () => FileDto })
+  @IsOptional()
+  photo?: FileDto | null;
+
+  @ApiPropertyOptional({ type: RoleDto })
+  @IsOptional()
+  @Type(() => RoleDto)
+  role?: RoleDto | null;
+
+  @ApiPropertyOptional({ type: StatusDto })
+  @IsOptional()
+  @Type(() => StatusDto)
+  status?: StatusDto;
+}
